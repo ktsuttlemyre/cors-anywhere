3fa4de96042772652c19d2cee9026feab4d8d519
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-10-02 21:32:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
+  getProxyForUrl:"reddit.com",
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
   //requireHeader: ['origin', 'x-requested-with'],
